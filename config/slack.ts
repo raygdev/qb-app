4e21caa84ffb26f04d441ff1ea0c9a5a60233857
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-04 19:50:09 -0400

    Configure slack for reuse

---
+++ config/slack.ts
@@ -0,0 +1,12 @@
+export const slackConfig = {
+    client_id: process.env.SLACK_CLIENT_ID!,
+    client_secret: process.env.SLACK_CLIENT_SECRET!,
+    authorization_endpoint: 'https://slack.com/oauth/v2/authorize',
+    scopes: [
+        'chat:write',
+        'chat:write.public',
+        'users:read',
+        'channels:read'
+    ],
+    redirect_uri: 'https://5333-2601-c2-e01-3b60-f862-8c15-901f-b27a.ngrok-free.app/slack/callback'
+}
