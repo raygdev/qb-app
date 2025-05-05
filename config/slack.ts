0eb087980aef1c590ee84a782183099df43e7463
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-04 21:21:46 -0400

    Add state redirect uri and token endpoint to config

--- config/slack.ts
+++ config/slack.ts
@@ -8,5 +8,7 @@ export const slackConfig = {
         'users:read',
         'channels:read'
     ],
-    redirect_uri: 'https://5333-2601-c2-e01-3b60-f862-8c15-901f-b27a.ngrok-free.app/slack/callback'
+    redirect_uri: 'https://5333-2601-c2-e01-3b60-f862-8c15-901f-b27a.ngrok-free.app/slack/callback',
+    state: 'state',
+    token_endpoint: 'https://slack.com/api/oauth.v2.access'
 }
