4ba8111bcf16cc1426f453c7249e8ad3ad9f4f9c
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-05 12:08:58 -0400

    Configure token revoke endpoint

--- config/slack.ts
+++ config/slack.ts
@@ -10,5 +10,6 @@ export const slackConfig = {
     ],
     redirect_uri: 'https://5333-2601-c2-e01-3b60-f862-8c15-901f-b27a.ngrok-free.app/slack/callback',
     state: 'state',
-    token_endpoint: 'https://slack.com/api/oauth.v2.access'
+    token_endpoint: 'https://slack.com/api/oauth.v2.access',
+    revocation_endpoint: 'https://slack.com/api/oauth.v2.revoke'
 }
