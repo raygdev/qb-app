98636d624c3e76bd9771c3fcd22b6d7f04bba4b2
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-06 15:22:25 -0400

    Add getPaymentById method to service

--- services/apis/quickbooks-api.ts
+++ services/apis/quickbooks-api.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 config()
 import axios, { Axios } from 'axios'
-import { QuickBooksInvoiceResponse } from "../../lib/types/quickbooks-types";
+import { QuickBooksInvoiceResponse, QuickBooksPaymentResponse } from "../../lib/types/quickbooks-types";
 
 const quickbooksClient = axios.create({
     baseURL: 'https://sandbox-quickbooks.api.intuit.com',
@@ -9,6 +9,7 @@ const quickbooksClient = axios.create({
 
 interface IQuickBooksService {
     getInvoiceById: (realmId: number, token: string, invoiceId: number) => Promise<QuickBooksInvoiceResponse['Invoice']>
+    getPaymentById: (realmId: number, token: string, paymentId: string) => Promise<QuickBooksPaymentResponse['Payment']>
 }
 
 class QuickBooksService implements IQuickBooksService {
@@ -20,7 +21,7 @@ class QuickBooksService implements IQuickBooksService {
 
   async getInvoiceById(realmId: number, token: string, invoiceId: number) {
     const { data } = await this.client.get<QuickBooksInvoiceResponse>(
-        `/v3/company/${realmId}/invoice/${invoiceId}`,
+        `/v3/company/${realmId}/invoice/${invoiceId}?minorversion=75`,
         { headers: {
             'Authorization': `Bearer ${token}`
         }}
@@ -29,6 +30,21 @@ class QuickBooksService implements IQuickBooksService {
     const  { Invoice }  = data 
 
     return Invoice
+  }
+
+  async getPaymentById(realmId: number, token: string, paymentId: string) {
+        const { data } = await this.client.get<QuickBooksPaymentResponse>(
+            `/v3/company/${realmId}/payment/${paymentId}?minorversion=75`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        )
+
+        const { Payment } = data
+
+        return Payment
   }
 }
 
