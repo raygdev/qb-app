1297ebf5f3b8545062a43df37a497ddc6c221e4f
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-07 16:40:13 -0400

    Add base setup for qb redirect

--- routes/auth/quickbooks/redirect.ts
+++ routes/auth/quickbooks/redirect.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from 'express'
+import { quickBooksAuth } from '../../../services/auth/qb-auth'
+
+// TODO: Make this an authenticated route for an app level user
+// buildAuth url should take in the token as state to authenticate in the callback
 
 export const quickbooksRedirect = (req: Request, res: Response) => {
-    res.send('NOT IMPLEMENTED')
+
+    const authUrl = quickBooksAuth.buildAuthUrl()
+
+    res.redirect(authUrl)
 }
