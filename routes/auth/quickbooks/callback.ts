55dc8d92787121fe444c575d4c749a033fdfd421
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-07 16:43:01 -0400

    Setup basic auth flow for quickbooks callback

--- routes/auth/quickbooks/callback.ts
+++ routes/auth/quickbooks/callback.ts
@@ -1,5 +1,14 @@
 import { Request, Response } from 'express'
+import { quickBooksAuth } from '../../../services/auth/qb-auth'
 
-export const quickbooksCallback = (req: Request, res: Response) => {
-    res.send('NOT IMPLEMENTED')
+export const quickbooksCallback = async (req: Request<{}, any, any, { code: string, state: string, realmId: string }>, res: Response) => {
+   const { code, state, realmId } = req.query
+
+   //TODO: validate state when it comes in (based on an authed user)
+
+   const tokens = await quickBooksAuth.getTokens(code, realmId)
+   // TODO: Store tokens with authed user if realmId doesn't already exist
+   // redirect back to client application DON'T SEND TOKENS BACK
+   res.send(tokens)
+    
 }
