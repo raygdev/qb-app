896a48f3efb8545a98421925db0593ed38a0dbee
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-07 16:07:31 -0400

    Build boiler plate quickbooks auth routes

---
+++ routes/auth/quickbooks/redirect.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express'
+
+export const quickbooksRedirect = (req: Request, res: Response) => {
+    res.send('NOT IMPLEMENTED')
+}
---
+++ routes/auth/quickbooks/refresh.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express'
+
+export const quickbooksRefresh = (req: Request, res: Response) => {
+    res.send('NOT IMPLEMENTED')
+}
