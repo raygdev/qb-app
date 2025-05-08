488523f4636fc256e3f0e260e34ea3f0d9130ded
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-07 21:57:20 -0400

    Clean up index and use api v1 router

--- index.ts
+++ index.ts
@@ -1,80 +1,15 @@
-import express, { Request } from 'express'
+import express from 'express'
 import { config } from 'dotenv'
 config()
-import { quickBooksAuth } from './services/auth/qb-auth'
-import { slackAuth } from './services/auth/slack-auth'
-import axios from 'axios'
 const app = express()
-
-interface QueryParams {
-    code: string,
-    realmId: string
-}
-
-interface CallbackQuery extends Request<{}, any, any, QueryParams>{}
+import { apiV1Router } from './routes'
 
 
 
-app.get('/', (req, res) => {
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-    const authUrl = quickBooksAuth.buildAuthUrl()
-
-    res.redirect(authUrl)
-
-})
-
-app.get('/callback', async (req: CallbackQuery, res) => {
-    const { code, realmId } = req.query
-
-    try {
-        const info = await quickBooksAuth.getTokens(code ,realmId)
-
-        res.json({ message:'success', ...info })
-
-    } catch(e: any) {
-        console.log(e.response.data)
-
-        res.end()
-    }
-    
-})
-
-app.get('/slack', (req, res) => {
-    const authUrl = slackAuth.buildAuthUrl()
-
-    res.redirect(authUrl)
-})
-
-app.get('/slack/callback', async (req: Request<{}, any, any, {code: string, state: string}>, res) => {
-    const { code, state } = req.query
-
-    if(!state) {
-        res.end()
-        return
-    }
-
-    const tokens = await slackAuth.getAccessToken(code,state)
-
-    res.send(tokens)
-})
-
-app.get('/user', async (req,res) => {
-
-    let token;
-    try {
-        const { data }  = await axios.get(process.env.QB_USERINFO_ENDPOINT!, {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        })
-
-        res.send(data)
-    } catch(e) {
-        console.log(e)
-
-        res.end()
-    }
-})
+app.use('/api/v1', apiV1Router)
 
 app.listen(3000, () => {
     console.log('app listening on port 3000')
