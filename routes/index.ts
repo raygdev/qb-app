887d5dc4a97e552bbdfa1643ea69bc0093ac0fce
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-07 22:34:16 -0400

    Hook up router for quickbooks endpoints

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { AuthRoutes } from './auth'
+import { QuickBooksRoutes } from './quickbooks'
 
 const apiV1Router = express.Router()
 
 new AuthRoutes(apiV1Router)
+new QuickBooksRoutes(apiV1Router)
 
 
 
