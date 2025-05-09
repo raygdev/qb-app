d558286a68c4974084c164c4a4aa35b391d92256
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-08 21:09:18 -0400

    Build out basic functionality for slack callback

--- routes/auth/slack/callback.ts
+++ routes/auth/slack/callback.ts
@@ -1,5 +1,37 @@
 import { Request, Response } from 'express'
+import { slackAuth } from '../../../services/auth/slack-auth'
 
-export const slackCallback = (req: Request, res: Response) => {
-    res.send('NOT IMPLEMENTED')
+interface SlackAuthQuery {
+    code?: string,
+    state?: string
+}
+
+export const slackCallback = async (req: Request<{}, any, any, SlackAuthQuery>, res: Response) => {
+    const { code, state } = req.query
+
+    if(!code || !state) {
+      res.status(401).send('Forbidden')
+      return;
+    }
+
+    /**
+     * @todo validate state (user token). If it fails, respond with 401
+     */
+
+    const tokens = await slackAuth.getAccessToken(code, state)
+
+    /**
+     * @todo don't send tokens back. Store them in their own team schema
+     * Check that a team exists
+     *      If it doesn't exist
+     *          create a new team and add token
+     *          associate the requesting user to the team token
+     *      If the team does exist...
+     *        If the user doesn't already have a team token assigned
+     *          assign the user a reference (id) to the team token schema
+     * 
+     * redirect back to client application
+     */
+
+    res.send(tokens)
 }
