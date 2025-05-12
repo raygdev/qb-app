c64d1970f2b4fe56d74c13ec5737238e843671c6
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-12 00:12:47 -0400

    Connect to mongo before server start

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { config } from 'dotenv'
+import mongoose from 'mongoose'
 config()
 const app = express()
 import { apiV1Router } from './routes'
@@ -11,6 +12,19 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1', apiV1Router)
 
-app.listen(3000, () => {
-    console.log('app listening on port 3000')
-})
+const dbUser = process.env.DB_USERNAME
+const dbPass = process.env.DB_PASSWORD
+
+const uri = `mongodb://${dbUser}:${dbPass}@mongo:27017/test?authSource=admin&retryWrites=true&w=majority`
+
+mongoose.connect(uri)
+ .then(() => {
+    console.log('connected successfully')
+    app.listen(3000, () => {
+        console.log('app listening on port 3000')
+    })
+ })
+ .catch(e => {
+    console.log(`ERROR CONNECTING: ${e}`)
+ })
+
