8baf6b41b09f9aeb2d54db17820494ff0badea23
raygdev <101662915+raygdev@users.noreply.github.com>
2025-05-12 00:05:19 -0400

    Add functionality to create and store company in callback

--- routes/auth/quickbooks/callback.ts
+++ routes/auth/quickbooks/callback.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { quickBooksAuth } from '../../../services/auth/qb-auth'
+import { findQuickbooksCompany, createQuickBooksCompany } from '../../../models/quickbooks'
 
 export const quickbooksCallback = async (req: Request<{}, any, any, { code: string, state: string, realmId: string }>, res: Response) => {
    const { code, state, realmId } = req.query
@@ -9,6 +10,23 @@ export const quickbooksCallback = async (req: Request<{}, any, any, { code: stri
    const tokens = await quickBooksAuth.getTokens(code, realmId)
    // TODO: Store tokens with authed user if realmId doesn't already exist
    // redirect back to client application DON'T SEND TOKENS BACK
-   res.send(tokens)
+
+   const company = await findQuickbooksCompany(tokens.realmId)
+
+   if(!company) {
+      const newCompany = await createQuickBooksCompany({
+         realmId: tokens.realmId,
+         refreshToken: tokens.refresh_token,
+         accessToken: tokens.access_token
+      })
+      res.send(newCompany)
+      return
+   }
+
+   company.set('accessToken', tokens.access_token)
+   company.set('refreshToken', tokens.refresh_token)
+
+   await company.save()
+   res.send(company)
     
 }
