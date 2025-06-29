ed7abfcff3128ec917c13fd90063e3c926717002
raygdev <101662915+raygdev@users.noreply.github.com>
2025-06-29 09:09:54 -0400

    Add slack routes to router

--- routes/index.ts
+++ routes/index.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import { AuthRoutes } from './auth'
 import { QuickBooksRoutes } from './quickbooks'
 import { UserRouter } from './users'
+import { SlackRoutes } from './slack'
 
 const apiV1Router = express.Router()
 
 new AuthRoutes(apiV1Router)
 new QuickBooksRoutes(apiV1Router)
 new UserRouter(apiV1Router)
+new SlackRoutes(apiV1Router)
 
 
 
