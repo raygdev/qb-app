785d84c8d96607bafaa454c1163f4ff7c5bdda0b
raygdev <101662915+raygdev@users.noreply.github.com>
2025-06-29 10:15:33 -0400

    Add new customer routes to quickbooks router

--- routes/quickbooks/index.ts
+++ routes/quickbooks/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import { quickbooksWebhooks } from './webhook'
+import { getCustomers } from './customers'
+import { getCustomer } from './get-customer'
+import { updateNotify } from './update-notify'
 
 export class QuickBooksRoutes {
     constructor(router: Router) {
         router.post('/quickbooks/webhook', quickbooksWebhooks)
+        router.get('/quickbooks/customers/:realmId', getCustomers)
+        router.get('/quickbooks/customer/:id', getCustomer)
+        router.put('/quickbooks/customer/notify/:id', updateNotify)
     }
 }
