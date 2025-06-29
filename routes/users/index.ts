d86cfb0d4fa190a512c75742fb62f98096026109
raygdev <101662915+raygdev@users.noreply.github.com>
2025-06-29 09:31:41 -0400

    Add get route to user router

--- routes/users/index.ts
+++ routes/users/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createUser } from "./create";
 import { updateUser } from "./update";
+import { getUser } from "./get";
 
 export class UserRouter {
     constructor(router: Router) {
         router.post('/user', createUser)
         router.put('/user/:id', updateUser)
+        router.get('/user/:id', getUser)
     }
 }
