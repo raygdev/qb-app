20a0db662e59b3d59e66835f944395f624d62bc1
raygdev <101662915+raygdev@users.noreply.github.com>
2025-06-29 15:08:04 -0400

    Add listeners to notify worker

--- workers/notify-user.ts
+++ workers/notify-user.ts
@@ -16,3 +16,29 @@ NotifyJobReturn,
         autorun: false
     }
 )
+
+notifyWorker.on('closed', () => {
+    console.log('closing notify worker')
+})
+
+notifyWorker.on('error', (e) =>{
+     console.log(`ERROR: ${e}`)
+ })
+
+ notifyWorker.on('completed', async (job) => {
+    console.log(job.returnvalue)
+ })
+
+ notifyWorker.on('ready', () => {
+    console.log('notify worker started')
+ })
+
+
+process.on('SIGTERM', async () => {
+    await notifyWorker.close()
+    process.exit(0)
+})
+process.on('SIGINT', async () => {
+    await notifyWorker.close()
+    process.exit(0)
+})
